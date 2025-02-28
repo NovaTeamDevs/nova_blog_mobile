5c3611d88b5c1b4efb83cc6cb48383a4ff30d85f
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2025-02-28 08:11:03 -0800

    impl single post controller

---
+++ lib/controllers/single_post_controller.dart
@@ -0,0 +1,44 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:get/get.dart';
+import 'package:nova_blog_mobile/core/constants/app_colors.dart';
+import 'package:nova_blog_mobile/core/services/web_service.dart';
+import 'package:nova_blog_mobile/models/post_model.dart';
+
+class SinglePostController extends GetxController {
+  SinglePostController({required this.id});
+  final int id;
+  final WebService _webService = WebService();
+  PostModel? post;
+  
+  Future<void> getPost() async {
+    final response = await _webService.getRequest(endPoint: "/post/$id",token: await _getToken());
+    
+    if(response.statusCode == 200) {
+      post = PostModel.fromJson(response.data["data"]);
+      update();
+    } else {
+      Get.back();
+      final errorMessage = response.data["message"];
+      Get.snackbar("خطا", errorMessage,backgroundColor: AppColors.readColor);
+    }
+  }
+
+  Future<void> likeOrDisLike() async {
+    final response = await _webService.postRequest(endPoint: "/post/like-dislike/$id",token: await _getToken());
+
+    if(response.statusCode == 200) {
+      post!.isLiked = !post!.isLiked!;
+      update();
+    } else {
+      final errorMessage = response.data["message"];
+      Get.snackbar("خطا", errorMessage,backgroundColor: AppColors.readColor);
+    }
+  }
+
+  Future<String?> _getToken() async {
+    final FlutterSecureStorage secureStorage = FlutterSecureStorage();
+    final token = await secureStorage.read(key: "token");
+    return token;
+  }
+    
+}
